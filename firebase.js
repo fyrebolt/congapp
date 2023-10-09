648fa548e227a69f9d37d8f3a3ac8586ff0f4b05
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2023-10-09 16:52:19 -0700

    g

---
+++ firebase.js
@@ -0,0 +1,23 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBh3lG5bakkiyeNnAPWhFnrL884vpLcLSc",
+    authDomain: "congressapp-67162.firebaseapp.com",
+    projectId: "congressapp-67162",
+    storageBucket: "congressapp-67162.appspot.com",
+    messagingSenderId: "579045935855",
+    appId: "1:579045935855:web:7673e2d8daf56dabbfa91a",
+    measurementId: "G-W6D45LZY7F"
+  }
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database()
+
+function check(){
+    if(localStorage.getItem("loggedIn")==null || localStorage.getItem("loggedIn")=="null"){
+        window.location.href = "index.html";
+    }
+}
+function checkIndex(){
+    if(localStorage.getItem("loggedIn")=="yes"){
+        window.location.href = "home.html";
+    }
+}
