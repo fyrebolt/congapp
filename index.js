dabb5455522cd26d5b55aa27df1ab73bba99319f
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2023-10-09 17:02:32 -0700

    ed

---
+++ index.js
@@ -0,0 +1,9 @@
+signUpButton = document.getElementById("signUp")
+logInButton = document.getElementById("logIn")
+
+signUpButton.onclick = () =>{
+    window.location.href=register.html;
+}
+logInButton.onclick = () =>{
+    window.location.href=login.html;
+}
