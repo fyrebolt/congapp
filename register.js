d8d09ba28e651ffc34358e03da967127e38af345
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2023-10-09 17:09:22 -0700

    j

---
+++ register.js
@@ -0,0 +1,40 @@
+const signupButton = document.getElementById('registerSubmit');
+const emailField = document.getElementById("emailField")
+const passwordField = document.getElementById("passwordField")
+const googleLogin = document.getElementById('googleLogin')
+
+signupButton.onclick = (event) =>{
+    event.preventDefault()
+    const email = emailField.value
+    const password = passwordField.value
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+        localStorage.setItem("loggedIn","yes")
+        user = email.replaceAll(".","").replaceAll("#","").replaceAll("$",'').replaceAll("[","").replaceAll("]","")
+        user = user.substring(0,user.indexOf("@"))
+        localStorage.setItem("username",user)
+        window.location.href = "home.html"    
+    })
+    .catch((error) => {
+        errorMessage = error.message
+        window.alert("Error: " + errorMessage);      
+    })
+}
+
+
+googleLogin.onclick = (event) => {
+    event.preventDefault()
+    provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        localStorage.setItem("loggedIn","yes")
+        profile = result.user.providerData[0];
+        email = profile.email;
+        user = email.replaceAll(".","").replaceAll("#","").replaceAll("$",'').replaceAll("[","").replaceAll("]","")
+        user = user.substring(0,user.indexOf("@"))
+        localStorage.setItem("username",user)
+        window.location.href = "home.html"  
+    }).catch(function(error) {
+        errorMessage = error.message;
+        window.alert("Error: " + errorMessage);   
+    });
+}
