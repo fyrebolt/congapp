61cac72425d33b60841fd7f420395f3f70c019bd
NOR4Wal12 <130193869+NOR4Wal12@users.noreply.github.com>
2023-10-11 17:42:12 -0700

    Update wiki.js

--- wiki.js
+++ wiki.js
@@ -1,7 +1,7 @@
 function hideBar() {
     var bar = document.getElementById("searchBar");
-    if (checkLogin() == false) {
+    //if (checkLogin() == false) {
         bar.style.display = "none";
-    }
+    //}
 
 }
