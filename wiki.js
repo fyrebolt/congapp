da814ee8423a2cb9122719ab8162a524812241a6
NOR4Wal12 <130193869+NOR4Wal12@users.noreply.github.com>
2023-10-11 17:55:13 -0700

    w

--- wiki.js
+++ wiki.js
@@ -1,7 +1,14 @@
-function hideBar() {
-    var bar = document.getElementById("searchBar");
-    //if (checkLogin() == false) {
-        bar.style.display = "none";
-    //}
-
+const searchButton = document.getElementById('searchBar');
+loginButton.onclick = (event) =>{
+    event.preventDefault()
+    searchButton.style.display = "none";
 }
+
+
+/*function hideBar() {
+    var bar = document.getElementById("searchBar");
+    if (checkLogin() == false) {
+        bar.style.display = "none";
+    }
+
+}*/
