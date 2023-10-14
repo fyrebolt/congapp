640806b2838c45e7b47206d87d4e92fa0023d93e
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2023-10-13 19:10:21 -0700

    d

--- wiki.js
+++ wiki.js
@@ -1,16 +1,16 @@
-const searchButton = document.getElementById('searchButton');
-var bar = document.getElementById("searchBar");
-searchButton.onclick = (event) =>{
-    event.preventDefault()
-    window.alert("button works");
-    bar.style.display = "none";
-}
+// const searchButton = document.getElementById('searchButton');
+// var bar = document.getElementById("searchBar");
+// searchButton.onclick = (event) =>{
+//     event.preventDefault()
+//     window.alert("button works");
+//     bar.style.display = "none";
+// }
 
 
-/*function hideBar() {
-    var bar = document.getElementById("searchBar");
-    if (checkLogin() == false) {
-        bar.style.display = "none";
-    }
+// /*function hideBar() {
+//     var bar = document.getElementById("searchBar");
+//     if (checkLogin() == false) {
+//         bar.style.display = "none";
+//     }
 
-}*/
+// }*/
