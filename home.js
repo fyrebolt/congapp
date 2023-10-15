85a2b417501112304474556ae07eaf83105a9d2a
HotSanta <101854144+HotSanta@users.noreply.github.com>
2023-10-15 15:09:31 -0700

    Update home.js
    Reps button

--- home.js
+++ home.js
@@ -2,3 +2,7 @@ profileButton = document.getElementById('profileButton')
 profileButton.onclick =()=>{
     window.location.href="profile.html"
 }
+repsButton = document.getElementById('representativesButton')
+repsButton.onclick =()=>{
+    window.location.href="representatives.html"
+}
