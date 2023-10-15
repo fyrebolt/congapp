dfe9dd15189e2d6723e6ed3c14d746a2a177021e
HotSanta <101854144+HotSanta@users.noreply.github.com>
2023-10-15 15:15:03 -0700

    Create representative.js

---
+++ representative.js
@@ -0,0 +1,5 @@
+//going to need input conversion possibly. like if api works with zip, then do address - > zip, etc
+searchButton = document.getElementById("searchButton")
+repsButton.onclick =()=>{
+    console.log("clicked")
+}
