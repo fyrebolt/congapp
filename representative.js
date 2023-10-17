f6b0fbdfb285981c1756a410e538b43596d1fbc4
NOR4Wal12 <130193869+NOR4Wal12@users.noreply.github.com>
2023-10-16 21:50:43 -0700

    hope

--- representative.js
+++ representative.js
@@ -4,6 +4,28 @@ function loadClient() {
         .then(function() { console.log("GAPI client loaded for API"); },
               function(err) { console.error("Error loading GAPI client for API", err); });
   }
+  function fetchImage(wikiLink){
+    const wikipediaLink = wikiLink; 
+
+    // Define the API endpoint
+    const apiUrl = `https://en.wikipedia.org/w/api.php?action=parse&page=${encodeURIComponent(wikipediaLink)}&format=json`;
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        const page = data.parse;
+        if (page && page.images && page.images.length > 0) {
+        const firstImage = page.images[0];
+        // You can use the 'firstImage' variable to construct the image URL.
+        return firstImage;
+        } else {
+        console.log('No images found on the Wikipedia page.');
+        }
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+  }
   // Make sure the client is loaded before calling this method.
   function execute() {
     return gapi.client.civicinfo.representatives.representativeInfoByAddress({
@@ -22,9 +44,12 @@ function loadClient() {
               },
               function(err) { console.error("Execute error", err); });
   }
-  gapi.load("client");
+
+
+gapi.load("client");
 searchInput = document.getElementById("searchInput")
+bar = document.getElementByClass("houseRepImage")
 searchInput.onclick = () => {
     execute()
-    
+    bar.src = fetchImage(response.result.officials[0].urls[1])
 }
