347f1ff4377ec8566282b22f96dfe35d46fc382d
HotSanta <101854144+HotSanta@users.noreply.github.com>
2023-10-18 17:49:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ signUpButton = document.getElementById("signUp")
 logInButton = document.getElementById("logIn")
 guestButton = document.getElementById("guest")
 signUpButton.onclick = () =>{
-    window.location.href=register.html;
+    window.location.href="register.html"
 }
 logInButton.onclick = () =>{
-    window.location.href=login.html;
+    window.location.href="login.html"
 }
 guestButton.onclick = () =>{
     sessionStorage.setItem("guest","yes")
-    window.location.href=home.html;
+    window.location.href="home.html"
 }
