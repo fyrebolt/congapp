d5990451909aa435bd933076c568711c1ac14d2f
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2023-10-19 15:12:29 -0700

    Update register.js

--- register.js
+++ register.js
@@ -11,9 +11,6 @@ signupButton.onclick = (event) =>{
     firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((userCredential) => {
         localStorage.setItem("loggedIn","yes")
-        user = email.replaceAll(".","").replaceAll("#","").replaceAll("$",'').replaceAll("[","").replaceAll("]","")
-        user = user.substring(0,user.indexOf("@"))
-        localStorage.setItem("username",user)
         window.location.href = "home.html"    
     })
     .catch((error) => {
@@ -29,11 +26,8 @@ googleLogin.onclick = (event) => {
     provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider).then(function(result) {
         localStorage.setItem("loggedIn","yes")
-        profile = result.user.providerData[0];
-        email = profile.email;
-        user = email.replaceAll(".","").replaceAll("#","").replaceAll("$",'').replaceAll("[","").replaceAll("]","")
-        user = user.substring(0,user.indexOf("@"))
-        localStorage.setItem("username",user)
+        //profile = result.user.providerData[0];
+        //email = profile.email;
         window.location.href = "home.html"  
     }).catch(function(error) {
         errorLabel.className="";
