6dec0467bcf7a34dfbb31c8d45756a48de572ba0
NOR4Wal12 <130193869+NOR4Wal12@users.noreply.github.com>
2023-10-18 18:07:49 -0700

    added wikipedia description

--- representative.js
+++ representative.js
@@ -6,25 +6,7 @@ function loadClient() {
   }
   function fetchImage(wikiLink){
     }
-  function getDesc(wikiLink){
-    //repName="Joe_Biden"
-    repName=wikiLink.substring(30)
-    fetch("https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=extracts&exchars=600&explaintext&titles=" + repName + "&format=json")
-    .then(function(response){return response.json();})
-    .then(function(response) {
-        page = response.query.pages;
-        page[Object.keys(page)[0]]
-        desc = page[Object.keys(page)[0]].extract;
-        descArray = desc.split(".");
-        newDesc = "";
-        for (i = 0; i < 3; i++) {
-          newDesc += descArray[i] + "."
-        }
-        console.log(newDesc);
-        return newDesc;
-    })
-    .catch(function(error){console.log(error);});
-  }
+  
 
   // Make sure the client is loaded before calling this method.
   function execute(level,inputLine, searchType, tagList) {
@@ -40,16 +22,30 @@ function loadClient() {
         .then(function(response) {
                 // Handle the results here (response.result has the parsed body).
             for(let tag = 0; tag < tagList.length;tag++){
-                console.log(response.result.officials[tag].name);
+                
                 description = response.result.officials[tag].urls[1];
-                console.log(description);
-                console.log(description.type);
-                if(description == undefined){
-                  description = response.result.officials[tag].urls[0];
-                }
-                console.log(description);
-                text = response.result.officials[tag].name + "      " + description;
-                document.getElementById(tagList[tag]).textContent = text;
+                description = description.substring(30)
+                //console.log(description);
+                  fetch("https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=extracts&exchars=600&explaintext&titles=" + description + "&format=json")
+                  .then(function(response){return response.json();})
+                  .then(function(response) {
+                      page = response.query.pages;
+                      page[Object.keys(page)[0]]
+                      desc = page[Object.keys(page)[0]].extract;
+                      descArray = desc.split(".");
+                      newDesc = "";
+                      //console.log(newDesc)
+                      for (i = 0; i < 3; i++) {
+                        newDesc += descArray[i] + "."
+                      }
+                      //console.log(newDesc);
+                      document.getElementById(tagList[tag]).innerHTML = newDesc;
+                  })
+                  .catch(function(error){console.log(error);});
+
+                text = response.result.officials[tag].name;
+                //console.log(text);
+                document.getElementById(tagList[tag]).innerHTML = "a";
             }
                 //logan: might work, change 0 to tag
                 //fetchImage(response.result.officials[0].urls[1])
