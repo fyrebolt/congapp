7c1fb3f765bd2d77d7836852b86ba23ff4371988
fyrebolt <82546290+fyrebolt@users.noreply.github.com>
2023-10-19 20:27:40 -0700

    clickable dropdown?
    pls work

--- wiki.js
+++ wiki.js
@@ -14,3 +14,13 @@
 //     }
 
 // }*/
+
+function toggleDropdown(button) {
+    var content = button.nextElementSibling;
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+
