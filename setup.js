ee6ae54e5e73bf63b5ba896f6b6bdd8a2c43108e
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2023-10-23 10:34:29 -0700

    Update setup.js

--- setup.js
+++ setup.js
@@ -8,6 +8,16 @@ submit = document.getElementById("continue")
 submit.onclick = (event) => {
     event.preventDefault()
     if(addressInput.value.trim() && cityInput.value.trim() && ((zipInput.value.length==5 && !isNaN(zipInput.value)) || !zipInput.value.trim()) && birthInput.value!=0){
-        console.log("works")
+        data={}
+        email = localStorage.getItem("user")
+        user = email.replaceAll(".","").replaceAll("#","").replaceAll("$",'').replaceAll("[","").replaceAll("]","")
+        user = user.substring(0,user.indexOf("@"))
+        data["address"] = addressInput.value
+        data["city"] = cityInput.value
+        data["zip"] = zipInput.value
+        data["birthdate"] = birthInput.value
+        database.ref(user+'/info').set(data).then(()=>{
+            console.log("data sent")
+        })
     }
 }
