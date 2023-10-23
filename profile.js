38ec2f5115260edb0bebb8855d2774a42f514d6f
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2023-10-23 11:04:39 -0700

    Update profile.js

--- profile.js
+++ profile.js
@@ -24,9 +24,23 @@ createButton.onclick = (event) =>{
 }
 
 emailLabel = document.getElementById("emailLabel")
+addressInput = document.getElementById("addressInput")
+cityInput = document.getElementById("cityInput")
+zipInput = document.getElementById("zipInput")
+birthInput = document.getElementById("birthInput")
 profileWrapper = document.getElementById("profileWrapper")
 if(sessionStorage.getItem("guest")!="yes"){
     emailLabel.innerHTML = localStorage.getItem("user")
+    email = localStorage.getItem("user")
+    user = email.replaceAll(".","").replaceAll("#","").replaceAll("$",'').replaceAll("[","").replaceAll("]","")
+    user = user.substring(0,user.indexOf("@"))
+    database.ref(user+'/info').once('value').then((snapshot)=>{ 
+        data = snapshot.val()
+        addressInput.value = data[data[0]]
+        cityInput.value = data[data[1]]
+        zipInput.value = data[data[2]]
+        birthInput.value = data[data[3]]
+    })
 }
 else{
     profileWrapper.style.display = "none";
