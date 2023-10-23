fff4de22cec2ee31769e2c9051d273d939a9a563
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2023-10-23 11:56:46 -0700

    Update profile.js

--- profile.js
+++ profile.js
@@ -46,3 +46,43 @@ else{
     profileWrapper.style.display = "none";
     guestWrapper.style.display = "flex";
 }
+editButton = document.getElementById("editProfile")
+editButton.onclick = (event) => {
+    event.preventDefault()
+    if(editButton.innerHTML == "Edit"){
+        editButton.innerHTML = "Save"
+        addressInput.disabled = "false"
+        cityInput.disabled = "false"
+        zipInput.disabled = "false"
+        birthInput.disabled = "false"
+    }
+    else{
+        addressInput.disabled = "true"
+        cityInput.disabled = "true"
+        zipInput.disabled = "true"
+        birthInput.disabled = "true"
+        if(addressInput.value.trim() && cityInput.value.trim() && ((zipInput.value.length==5 && !isNaN(zipInput.value)) || !zipInput.value.trim()) && birthInput.value!=0){
+            data={}
+            email = localStorage.getItem("user")
+            user = email.replaceAll(".","").replaceAll("#","").replaceAll("$",'').replaceAll("[","").replaceAll("]","")
+            user = user.substring(0,user.indexOf("@"))
+            data["address"] = addressInput.value
+            data["city"] = cityInput.value
+            data["zip"] = zipInput.value
+            data["birthdate"] = birthInput.value
+            database.ref(user+'/info').set(data).then(()=>{
+                database.ref(user+'/info').once('value').then((snapshot)=>{ 
+                data = snapshot.val()
+                addressInput.value = data.address
+                cityInput.value = data.city
+                zipInput.value = data.zip
+                birthInput.value = data.birthdate
+            })
+            })
+        }
+        else{
+            console.log("Please enter valid information")
+        }
+        editButton.innerHTML = "Edit"
+    }
+}
