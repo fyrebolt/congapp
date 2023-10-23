d36f63ed3f817f65da902163691f96eab84eb6f6
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2023-10-22 19:17:32 -0700

    fix profile shit

--- profile.js
+++ profile.js
@@ -1,10 +1,27 @@
-logoutButton = document.getElementById("logout")
+logoutButton = document.getElementById("logout1")
 logoutButton.onclick = () =>{
     localStorage.setItem("loggedIn", "null")
     sessionStorage.setItem("guest", "null")
     localStorage.setItem("user", "null")
     window.location.href="index.html"
 }
+
+logoutButton2 = document.getElementById("logout2")
+logoutButton2.onclick = () =>{
+    localStorage.setItem("loggedIn", "null")
+    sessionStorage.setItem("guest", "null")
+    localStorage.setItem("user", "null")
+    window.location.href="index.html"
+}
+
+createButton = document.querySelector("a")
+createButton.onclick = (event) =>{
+    event.preventDefault()
+    localStorage.setItem("loggedIn", "null")
+    sessionStorage.setItem("guest", "null")
+    localStorage.setItem("user", "null")
+}
+
 emailLabel = document.getElementById("emailLabel")
 profileWrapper = document.getElementById("profileWrapper")
 if(sessionStorage.getItem("guest")!="yes"){
