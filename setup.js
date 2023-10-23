459528bdfe46c6669bba2b0f10c119179703e02a
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2023-10-23 17:09:28 +0000

    Update setup.js

--- setup.js
+++ setup.js
@@ -2,12 +2,12 @@ addressInput = document.getElementById("addressInput")
 cityInput = document.getElementById("cityInput")
 zipInput = document.getElementById("zipInput")
 birthInput = document.getElementById("birthInput")
+//yyyy-mm-dd
+numbers='0123456789'
 submit = document.getElementById("continue")
 submit.onclick = (event) => {
     event.preventDefault()
-    console.log(addressInput.value)
-    console.log(cityInput.value)
-    console.log(zipInput.value)
-    console.log(zipInput.value.trim())
-    console.log(birthInput.value)
+    if(addressInput.value.trim() && cityInput.value.trim() && ((zipInput.value.length()==5 && !isNaN(zipInput.value)) || !zipInput.value.trim()) && birthInput.value!=0){
+        console.log("works")
+    }
 }
