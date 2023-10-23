71f8d1428ec9f82501a236cb710448150a9d8420
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2023-10-23 08:16:52 -0700

    Update setup.js

---
+++ setup.js
@@ -0,0 +1,9 @@
+addressInput = document.getElementById("addressInput")
+cityInput = document.getElementById("cityInput")
+zipInput = document.getElementById("zipInput")
+birthInput = document.getElementById("birthInput")
+submit = document.getElementById("continue")
+continue.onlick = (event) => {
+    event.preventDefault()
+    
+}
