f37f5bd2255a15c954d31a636f163d8fee41d610
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2023-10-23 10:19:23 -0700

    Update setup.js

--- setup.js
+++ setup.js
@@ -7,7 +7,7 @@ numbers='0123456789'
 submit = document.getElementById("continue")
 submit.onclick = (event) => {
     event.preventDefault()
-    if(addressInput.value.trim() && cityInput.value.trim() && ((zipInput.value.length()==5 && !isNaN(zipInput.value)) || !zipInput.value.trim()) && birthInput.value!=0){
+    if(addressInput.value.trim() && cityInput.value.trim() && ((zipInput.value.length==5 && !isNaN(zipInput.value)) || !zipInput.value.trim()) && birthInput.value!=0){
         console.log("works")
     }
 }
