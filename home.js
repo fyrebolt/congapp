95061a7f5f9a994942daa56323182f996ec09c8e
NOR4Wal12 <130193869+NOR4Wal12@users.noreply.github.com>
2023-10-29 16:33:25 -0700

    testing keypresses

--- home.js
+++ home.js
@@ -1,3 +1,8 @@
+dave=document.getElementById("dave");
+window.addEventListener('keydown', function (e) {
+    dave.innerHTML = `You pressed ${e.key}`;
+  }, false);
+
 profileButton = document.getElementById('profileButton')
 profileButton.onclick =()=>{
     window.location.href="profile.html"
