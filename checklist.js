50c8fb1a5b4b9ed9f9102f73b1aec2e06fb0fb1b
ziadm9 <117851167+ziadm9@users.noreply.github.com>
2023-10-29 14:32:35 -0700

    s

--- checklist.js
+++ checklist.js
@@ -26,15 +26,15 @@ if(sessionStorage.getItem("guest")!="yes"){
     user = user.substring(0,user.indexOf("@"))
     database.ref(user+'/checklist').once('value').then((snapshot)=>{ 
         data = snapshot.val()
-        o1.value = data.option1;
-        o2.value = data.option2;
-        o3.value = data.option3;
-        o4.value = data.option4;
-        o5.value = data.option5;
-        o6.value = data.option6;
-        o7.value = data.option7;
-        o8.value = data.option8;
-        o9.value = data.option9;
+        o1.checked = data.option1;
+        o2.checked = data.option2;
+        o3.checked = data.option3;
+        o4.checked = data.option4;
+        o5.checked = data.option5;
+        o6.checked = data.option6;
+        o7.checked = data.option7;
+        o8.checked = data.option8;
+        o9.checked = data.option9;
     })
 }
 else{
@@ -44,33 +44,31 @@ else{
 
 editButton = document.getElementById("editProfile")
 editButton.onclick = (event) => {
-            data={}
-            data["option1"] = o1.value;
-            data["option2"] = o2.value;
-            data["option3"] = o3.value;
-            data["option4"] = o4.value;
-            data["option5"] = o5.value;
-            data["option6"] = o6.value;
-            data["option7"] = o7.value;
-            data["option8"] = o8.value;
-            data["option9"] = o9.value;
-            
-            
-            database.ref(user+'/checklist').set(data).then(()=>{
-                email = localStorage.getItem("user")
-                user = email.replaceAll(".","").replaceAll("#","").replaceAll("$",'').replaceAll("[","").replaceAll("]","")
-                user = user.substring(0,user.indexOf("@"))
-                database.ref(user+'/checklist').once('value').then((snapshot)=>{ 
-                data = snapshot.val()
-                o1.value = data.option1;
-                o2.value = data.option2;
-                o3.value = data.option3;
-                o4.value = data.option4;
-                o5.value = data.option5;
-                o6.value = data.option6;
-                o7.value = data.option7;
-                o8.value = data.option8;
-                o9.value = data.option9;
-            })
-            })
+    data={}
+    data["option1"] = o1.checked;
+    data["option2"] = o2.checked;
+    data["option3"] = o3.checked;
+    data["option4"] = o4.checked;
+    data["option5"] = o5.checked;
+    data["option6"] = o6.checked;
+    data["option7"] = o7.checked;
+    data["option8"] = o8.checked;
+    data["option9"] = o9.checked;
+    database.ref(user+'/checklist').set(data).then(()=>{
+        email = localStorage.getItem("user")
+        user = email.replaceAll(".","").replaceAll("#","").replaceAll("$",'').replaceAll("[","").replaceAll("]","")
+        user = user.substring(0,user.indexOf("@"))
+        database.ref(user+'/checklist').once('value').then((snapshot)=>{ 
+            data = snapshot.val()
+            o1.checked = data.option1;
+            o2.checked = data.option2;
+            o3.checked = data.option3;
+            o4.checked = data.option4;
+            o5.checked = data.option5;
+            o6.checked = data.option6;
+            o7.checked = data.option7;
+            o8.checked = data.option8;
+            o9.checked = data.option9;
+        })
+    })
 }
